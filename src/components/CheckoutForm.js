1626f56e642ef736dd9fedc6182ef4d918dfc11b
Jason Vance <jbvance@gmail.com>
2023-03-21 11:30:09 -0500

    update styling

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -106,6 +106,7 @@ export default function CheckoutForm() {
             />
             <Button
               type="submit"
+              className="StripeSubmitButton"
               disabled={isLoading || !stripe || !elements}
               id="submit"
             >
