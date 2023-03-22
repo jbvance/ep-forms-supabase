7f13dd6ee988e47193f1fd9aa0310d85420da2ae
Jason Vance <jbvance@gmail.com>
2023-03-22 07:03:54 -0500

    refactor checkout to use stripe api call as form submission

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,134 +1,35 @@
 import React from 'react';
-import {
-  PaymentElement,
-  LinkAuthenticationElement,
-  useStripe,
-  useElements,
-} from '@stripe/react-stripe-js';
-import { Container, Row, Col } from 'react-bootstrap';
+import { loadStripe } from '@stripe/stripe-js';
 
-import Spinner from 'react-bootstrap/Spinner';
-import Button from 'react-bootstrap/Button';
-
-export default function CheckoutForm() {
-  const stripe = useStripe();
-  const elements = useElements();
-  console.log('ELEMENTS', elements);
-
-  const [email, setEmail] = React.useState('');
-  const [message, setMessage] = React.useState(null);
-  const [isLoading, setIsLoading] = React.useState(false);
-
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+const CheckoutForm = () => {
   React.useEffect(() => {
-    if (!stripe) {
-      return;
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.');
     }
 
-    const clientSecret = new URLSearchParams(window.location.search).get(
-      'payment_intent_client_secret'
-    );
-
-    if (!clientSecret) {
-      return;
+    if (query.get('canceled')) {
+      console.log(
+        'Order canceled -- continue to shop around and checkout when you’re ready.'
+      );
     }
-
-    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-      switch (paymentIntent.status) {
-        case 'succeeded':
-          setMessage('Payment succeeded!');
-          break;
-        case 'processing':
-          setMessage('Your payment is processing.');
-          break;
-        case 'requires_payment_method':
-          setMessage('Your payment was not successful, please try again.');
-          break;
-        default:
-          setMessage('Something went wrong.');
-          break;
-      }
-    });
-  }, [stripe]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
-
-    setIsLoading(true);
-
-    const { error } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        // Make sure to change this to your payment completion page
-        return_url: process.env.NEXT_PUBLIC_STRIPE_RETURN_URL,
-      },
-    });
-
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === 'card_error' || error.type === 'validation_error') {
-      setMessage(error.message);
-    } else {
-      setMessage('An unexpected error occurred.');
-    }
-
-    setIsLoading(false);
-  };
-
-  const paymentElementOptions = {
-    layout: 'tabs',
-    defaultValues: {
-      billingDetails: {
-        name: 'Jason',
-      },
-    },
-  };
+  }, []);
 
   return (
-    <Container style={{ border: '1px solid gray' }}>
-      <Row>
-        <Col className="StripeFormHeader">
-          Enter your Payment Information below
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <form id="payment-form" onSubmit={handleSubmit}>
-            {/* <LinkAuthenticationElement
-          id="link-authentication-element"
-          onChange={(e) => setEmail(e.target.value)}
-  />*/}
-            <PaymentElement
-              id="payment-element"
-              options={paymentElementOptions}
-            />
-            <Button
-              type="submit"
-              className="StripeSubmitButton"
-              disabled={isLoading || !stripe || !elements}
-              id="submit"
-            >
-              <span id="button-text">
-                {isLoading ? <Spinner /> : 'Pay now'}
-              </span>
-            </Button>
-            {/* Show any error or success messages */}
-            {message && (
-              <div className="CheckoutErrorMessage" id="payment-message">
-                {message}
-              </div>
-            )}
-          </form>
-        </Col>
-      </Row>
-    </Container>
+    <form action="/api/checkout-sessions" method="POST">
+      <section>
+        <button type="submit" role="link">
+          Checkout
+        </button>
+      </section>
+    </form>
   );
-}
+};
+
+export default CheckoutForm;
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,58 +1,8 @@
-import React, { useEffect } from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import React from 'react';
 import CheckoutForm from 'components/CheckoutForm';
-import supabase from 'util/supabase';
 
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-);
-
-const CheckoutPage = (props) => {
-  const [clientSecret, setClientSecret] = React.useState('');
-
-  useEffect(() => {
-    const createPaymentIntent = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      const accessToken = session ? session.access_token : '';
-      //Create PaymentIntent as soon as the page loads
-      fetch('/api/create-payment-intent', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({ items: [{ id: 'xl-tshirt' }] }),
-      })
-        .then((res) => res.json())
-        .then((data) => setClientSecret(data.clientSecret));
-    };
-
-    createPaymentIntent();
-  }, []);
-
-  const appearance = {
-    theme: 'stripe',
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
-
-  return (
-    <div className="App">
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      )}
-    </div>
-  );
+const CheckoutPage = () => {
+  return <CheckoutForm />;
 };
 
 export default CheckoutPage;
